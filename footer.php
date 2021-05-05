bd12120456a298cf66b925744ea6378148c9a245
v0idxyz <58184010+v0idxyz@users.noreply.github.com>
2021-05-05 23:37:45 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<div style="display:block;" class="d-block w-100 container-fluid SecondLayer">
 			<div class="row justify-content-center">
-				<p>Powered by some Autofaucet Script on GitHub</p>
+				<p>Powered by some <a href="https://c2g.at">Some Dude that runs a paying Shortlink Service</a></p>
 			</div>
 		</div>
 	</body>
