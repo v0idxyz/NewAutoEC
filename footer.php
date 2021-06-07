7973f3d8b4b2b1415da72e09b82703e46c4c663d
v0idxyz <58184010+v0idxyz@users.noreply.github.com>
2021-06-07 21:47:24 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 		<div style="display:block;" class="d-block w-100 container-fluid SecondLayer">
 			<div class="row justify-content-center">
 				<p>Powered by some <a href="https://c2g.at">Some Dude that runs a paying Shortlink Service</a></p>
+				<p>Get the Script on Github: <a href="https://github.com/v0idxyz/NewAutoEC">Click here</a></p>
 			</div>
 		</div>
 	</body>
